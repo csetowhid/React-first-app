3a6373392dc5ba7816a3211c2963ae249f792d58
csetowhid <towhidhasang1@gmail.com>
2022-10-20 07:42:44 +0600

    Event Handle

---
+++ src/components/Event-Binding/Index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+
+class EventBonding extends Component{
+    // constructor(props){
+    //     super(props)
+    //     this.state  = {
+    //         changedValue : ''
+    //     }
+    // }
+
+    // handleOnChnage = (e) => {
+    //     this.setState({
+    //         changedValue: e.target.value
+    //     }, ()=>{
+    //         console.log(this.state.changedValue);
+    //     })
+    // }
+
+    render() {
+        
+        return (
+            <div>
+                <h1>Hello Event Bnding </h1>
+            </div>
+        )
+        
+    }
+}
+
+export default EventBonding;
