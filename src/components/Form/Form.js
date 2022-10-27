ed8a42591705f3111b9c9ddb47dc8b41009a9d35
csetowhid <towhidhasang1@gmail.com>
2022-10-27 12:07:48 +0600

    useState with object

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,23 +2,48 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export default function Form(){
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
 
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    }
+    const [user, setUser] = useState({name : '', email: ''});
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    }
+    const {name, email} = user;
+
+    //  const handleNameChange = (e) => {
+    //     setUser({name: e.target.value, email});
+    //  }
+
+    //  const handleEmailChange = (e) => {
+    //     setUser({name, email : e.target.value});
+    //  }
+
+     const handlChange = (e) => {
+        const fieldName = e.target.name;
+
+        setUser({...user,[e.target.name]:e.target.value});
+
+        // if(fieldName === 'name'){
+        //     setUser({name: e.target.value, email});
+        // }
+        // if(fieldName === 'email'){
+        //     setUser({name, email : e.target.value});
+        // }
+        e.preventDefault();
+     }
+    // const handleNameChange = (e) => {
+    //     setName(e.target.value);
+    // }
+
+    // const handleEmailChange = (e) => {
+    //     setEmail(e.target.value);
+    // }
     const handleSubmit = (e) => {
         console.log('Form Is Submitted');
-        let userInfo ={
-            name,
-            email
-        }
-        console.log(userInfo);
+        // let userInfo ={
+        //     name,
+        //     email
+        // }
+        console.log(user);
         e.preventDefault();
     }
     return (
@@ -27,11 +52,13 @@ export default function Form(){
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>Name</label>
-                    <input type="text" name="name" value={name} onChange={handleNameChange} />
+                    {/* <input type="text" name="name" value={name} onChange={handleNameChange} /> */}
+                    <input type="text" name="name" value={name} onChange={handlChange} />
                 </div>
                 <div>
                     <label>Email</label> 
-                    <input type="email" name="email" value={email} onChange={handleEmailChange} />
+                    {/* <input type="email" name="email" value={email} onChange={handleEmailChange} /> */}
+                    <input type="email" name="email" value={email} onChange={handlChange} />
                 </div>
                 <div>
                     <Button type="submit">Save</Button>
